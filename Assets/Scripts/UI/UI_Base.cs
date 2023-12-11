353ecf65711e75c98309dd978b73ca3692e50c79
shstcs <shstcs@naver.com>
2023-12-11 22:29:58 +0900

    [Feature] (12.11) MainScene, Concept
    -메인 씬 UI 디자인
    -UI Binding 시도

---
+++ Assets/Scripts/UI/UI_Base.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Diagnostics;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public abstract class UI_Base : MonoBehaviour
+{
+    protected Dictionary<Type, UnityEngine.Object[]> _objects = new Dictionary<Type, UnityEngine.Object[]>();
+    public abstract void Init();
+
+    protected void Bind<T>(Type type) where T : UnityEngine.Object
+    {
+        string[] names = Enum.GetNames(type);
+        UnityEngine.Object[] objects = new UnityEngine.Object[names.Length];
+        _objects.Add(typeof(T), objects); // Dictionary 에 추가
+
+        // T 에 속하는 오브젝트들을 Dictionary의 Value인 objects 배열의 원소들에 하나하나 추가
+        for (int i = 0; i < names.Length; i++)
+        {
+            if (typeof(T) == typeof(GameObject))
+                objects[i] = Util.FindChild(gameObject, names[i], true);
+            else
+                objects[i] = Util.FindChild<T>(gameObject, names[i], true);
+
+            if (objects[i] == null)
+                Debug.Log($"Failed to bind({names[i]})");
+        }
+    }
+
+    public static void BindEvent(GameObject go, Action<PointerEventData> action, Define.UIEvent type = Define.UIEvent.Click)
+    {
+        UI_EventHandler evt = Util.GetOrAddComponent<UI_EventHandler>(go);
+
+        switch (type)
+        {
+            case Define.UIEvent.Click:
+                evt.OnClickHandler -= action; // 혹시나 이미 있을까봐 빼줌
+                evt.OnClickHandler += action;
+                break;
+
+            case Define.UIEvent.Drag:
+                evt.OnDragHandler -= action; // 혹시나 이미 있을까봐 빼줌
+                evt.OnDragHandler += action;
+                break;
+        }
+    }
+
+    protected T Get<T>(int idx) where T : UnityEngine.Object
+    {
+        UnityEngine.Object[] objects = null;
+        if (_objects.TryGetValue(typeof(T), out objects) == false)
+            return null;
+
+        return objects[idx] as T;
+    }
+
+    protected GameObject GetObject(int idx) { return Get<GameObject>(idx); } // 오브젝트로서 가져오기
+    protected Text GetText(int idx) { return Get<Text>(idx); } // Text로서 가져오기
+    protected Button GetButton(int idx) { return Get<Button>(idx); } // Button로서 가져오기
+    protected Image GetImage(int idx) { return Get<Image>(idx); } // Image로서 가져오기
+}
---
+++ Assets/Scripts/UI/UI_Main.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+using Image = UnityEngine.UI.Image;
+
+public class UI_Main : UI_Scene
+{
+    enum Texts
+    {
+        Title,
+        GoldText
+    }
+
+    enum Images
+    {
+        Background,
+        GoldImage
+    }
+
+    enum GameObjects
+    {
+        Gold
+    }
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public override void Init()
+    {
+        base.Init(); // UI_Button 의 부모인 UI_PopUp 의 Init() 호출
+
+        Bind<Text>(typeof(Texts));  // 텍스트 오브젝트들 가져와 dictionary인 _objects에 바인딩. 
+        Bind<GameObject>(typeof(GameObjects));  // 빈 오브젝트들 가져와 dictionary인 _objects에 바인딩. 
+        Bind<Image>(typeof(Images));  // 이미지 오브젝트들 가져와 dictionary인 _objects에 바인딩. 
+
+        // 이미지(go)에 UI_EventHandler를 붙이고 파라미터로 넘긴 람다 함수를 OnDragHandler 액션에 등록한다.
+        GameObject go = GetImage((int)Images.Background).gameObject;
+        BindEvent(go, (PointerEventData data) => { go.transform.position = data.position; }, Define.UIEvent.Drag);
+    }
+
+}
